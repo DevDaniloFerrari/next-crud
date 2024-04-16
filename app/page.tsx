b30efa7d8d855d2dbe31df0f1bddf54b13272c2a
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2024-04-16 07:46:34 -0300

    criando componente tabela

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,36 @@
+"use client";
 import Layout from "@/components/Layout";
+import Tabela from "@/components/Tabela";
+import Cliente from "@/core/Cliente";
 
 export default function Home() {
+  const clientes = [
+    new Cliente("Ana", 34, "1"),
+    new Cliente("Bia", 45, "2"),
+    new Cliente("Carlos", 23, "3"),
+    new Cliente("Pedro", 54, "4"),
+  ];
+
+  function clienteSelecionado(cliente: Cliente) {
+    console.log(cliente.nome);
+  }
+
+  function clienteExcluido(cliente: Cliente) {
+    console.log(cliente.nome);
+  }
+
   return (
-    <div className={`flex justify-center items-center h-screen
+    <div
+      className={`flex justify-center items-center h-screen
     bg-gradient-to-r from-blue-500 to-purple-500
-    text-white`}>
+    text-white`}
+    >
       <Layout titulo="Cadastro Simples">
-        <span>Conteudo</span>
+        <Tabela
+          clientes={clientes}
+          clienteSelecionado={clienteSelecionado}
+          clienteExcluido={clienteExcluido}
+        ></Tabela>
       </Layout>
     </div>
   );
